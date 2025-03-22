b035fc42b077568b5edf1dc7a52e0c94a5ea7dc4
HYEON <titeotty12@gmail.com>
2025-03-22 23:25:30 +0900

    chore: index.html 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
