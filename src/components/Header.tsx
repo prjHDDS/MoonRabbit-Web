ca4eb5c133e35224441d62da5a4fca08288e5c03
HYEON <titeotty12@gmail.com>
2025-03-27 11:37:30 +0900

    feat: Header 뼈대 구성

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React, { JSX, useState, useEffect, useRef } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+export const Header = (): JSX.Element => {
+    return (
+        <>
+        <div className='relative'>
+            <div className='fixed top-0 left-0 right-0'>
+
+            </div>
+        </div>
+        </>
+    )
+}
