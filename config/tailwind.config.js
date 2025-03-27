58c4304f20263f8f0b8b64b76b38eddf7e35fcda
HYEON <titeotty12@gmail.com>
2025-03-27 12:35:05 +0900

    build: 컬러팔레트세팅

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
       "./public/index.html",
     ],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          mainColor: 'E25F47',
+          lightBeige: '#FDF7EA',
+          darkBeige: '#F0D093',
+          lightCaramel: 'D48C5B',
+          lightWalnut: '937066',
+          darkWalnut: '473C2C',
+
+          mainWhite: 'F9F9FA',
+          subWhite: 'F5F5F5',
+          mainBlack: '17171B',
+          subBlack: '26262D',
+          mainGray: 'A8A8A8',
+        }
+      },
     },
     plugins: [
       require("tailwindcss-animate"), 
