98d9cb6046e278dc682df3a6751952ecfdfd9784
HYEON <74049556+titeotty@users.noreply.github.com>
2025-03-28 14:14:18 +0900

    docs: 오타수정

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -6,18 +6,18 @@ module.exports = {
     theme: {
       extend: {
         colors: {
-          mainColor: 'E25F47',
+          mainColor: '#E25F47',
           lightBeige: '#FDF7EA',
           darkBeige: '#F0D093',
-          lightCaramel: 'D48C5B',
-          lightWalnut: '937066',
-          darkWalnut: '473C2C',
+          lightCaramel: '#D48C5B',
+          lightWalnut: '#937066',
+          darkWalnut: '#473C2C',
 
-          mainWhite: 'F9F9FA',
-          subWhite: 'F5F5F5',
-          mainBlack: '17171B',
-          subBlack: '26262D',
-          mainGray: 'A8A8A8',
+          mainWhite: '#F9F9FA',
+          subWhite: '#F5F5F5',
+          mainBlack: '#17171B',
+          subBlack: '#26262D',
+          mainGray: '#A8A8A8',
         },
         fontFamily: {
           mainFont: ['HSBombaram2.1'],
